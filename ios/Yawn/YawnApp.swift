658246a136dc69b29a87e874c19895533a1510b9
Stefan van den Oord <info@strohmnative.dev>
2021-10-18 22:09:42 +0200

    Setup Strohm Native in app

--- ios/Yawn/YawnApp.swift
+++ ios/Yawn/YawnApp.swift
@@ -1,7 +1,10 @@
 import SwiftUI
+import StrohmNative
 
 @main
 struct YawnApp: App {
+    let strohmNative = StrohmNative.default
+
     var body: some Scene {
         WindowGroup {
             ContentView()
