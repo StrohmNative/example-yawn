cd79dee62a1ad9ef4fc47e5d524a226c1434ed57
Stefan van den Oord <info@strohmnative.dev>
2021-10-18 22:11:30 +0200

    Fix: reference strohm native from CLJS
    Otherwise it will be optimized away by the compiler and running things will fail

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,7 +1,12 @@
-(ns app.main)
+(ns app.main
+  (:require [strohm-native.flow :refer [create-store]]))
+
+;; Reducer doesn't do anything yet, simply returns state
+(defn reducer [state _action] state)
 
 (defn ^:export main! []
-  (tap> "Hello world!"))
+  (tap> "Hello world!")
+  (create-store reducer :initial-state {:greeting "Hello world!"}))
 
 (defn ^:dev/after-load reload! []
   (tap> "reloaded"))
